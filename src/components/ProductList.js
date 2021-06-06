a9da9e729235f9e5861a5466965a167928618032
Ahmad1431997 <ahmadabuawed@gmail.com>
2021-06-06 12:44:39 +0300

    searchBar

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -3,17 +3,25 @@ import { ListWrapper } from "../styles";
 // Components
 import ProductItem from "./ProductItem";
 import SearchBar from "./SearchBar";
+import { useState } from "react";
+
 // Data
 import products from "../products";
 
 const ProductList = () => {
-  const productList = products.map((product) => (
+  const [query, setQuery] = useState("");
+  const searchArray = products.filter((product) =>
+    product.name.toUpperCase().includes(query.toUpperCase())
+  );
+  // console.log({SearchArray});
+
+  const productList = searchArray.map((product) => (
     <ProductItem product={product} key={product.id} />
   ));
 
   return (
     <>
-      <SearchBar />
+      <SearchBar setQuery={setQuery} />
       <ListWrapper>{productList}</ListWrapper>
     </>
   );
